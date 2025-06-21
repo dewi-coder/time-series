e4682362a76952f4c6cbc2506fcfeec5a59ed053
dewi-coder <herlinitsme@gmail.com>
2025-06-21 10:18:44 +0700

    Add files

---
+++ main.py
@@ -0,0 +1,5 @@
+from time_series_visualizer import draw_line_plot, draw_bar_plot, draw_box_plot
+
+draw_line_plot().savefig("line_plot.png")
+draw_bar_plot().savefig("bar_plot.png")
+draw_box_plot().savefig("box_plot.png")
